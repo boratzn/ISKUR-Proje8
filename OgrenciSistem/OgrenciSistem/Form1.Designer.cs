c899a1eee9650645422fd1fd5dd7d977fd5c7975
Bora TÜZÜN <boratzn94@gmail.com>
2022-02-25 01:59:07 +0300

    Add files via upload

---
+++ OgrenciSistem/OgrenciSistem/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace OgrenciSistem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnTopluSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.cbxSinif = new System.Windows.Forms.ComboBox();
+            this.cbxDers = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbOgrAd = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbOgrNo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(16, 212);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(768, 218);
+            this.listView1.TabIndex = 26;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.Click += new System.EventHandler(this.listView1_Click);
+            // 
+            // btnTopluSil
+            // 
+            this.btnTopluSil.Location = new System.Drawing.Point(547, 166);
+            this.btnTopluSil.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTopluSil.Name = "btnTopluSil";
+            this.btnTopluSil.Size = new System.Drawing.Size(109, 28);
+            this.btnTopluSil.TabIndex = 22;
+            this.btnTopluSil.Text = "Seçilenleri Sil";
+            this.btnTopluSil.UseVisualStyleBackColor = true;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(424, 166);
+            this.btnGuncelle.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(100, 28);
+            this.btnGuncelle.TabIndex = 23;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(301, 166);
+            this.btnSil.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(100, 28);
+            this.btnSil.TabIndex = 24;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(181, 166);
+            this.btnEkle.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(100, 28);
+            this.btnEkle.TabIndex = 25;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // cbxSinif
+            // 
+            this.cbxSinif.FormattingEnabled = true;
+            this.cbxSinif.Location = new System.Drawing.Point(389, 124);
+            this.cbxSinif.Margin = new System.Windows.Forms.Padding(4);
+            this.cbxSinif.Name = "cbxSinif";
+            this.cbxSinif.Size = new System.Drawing.Size(187, 24);
+            this.cbxSinif.TabIndex = 17;
+            // 
+            // cbxDers
+            // 
+            this.cbxDers.FormattingEnabled = true;
+            this.cbxDers.Location = new System.Drawing.Point(389, 89);
+            this.cbxDers.Margin = new System.Windows.Forms.Padding(4);
+            this.cbxDers.Name = "cbxDers";
+            this.cbxDers.Size = new System.Drawing.Size(187, 24);
+            this.cbxDers.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(268, 127);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 16);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Sınıf";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(268, 93);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 16);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Ders";
+            // 
+            // tbOgrAd
+            // 
+            this.tbOgrAd.Location = new System.Drawing.Point(389, 55);
+            this.tbOgrAd.Margin = new System.Windows.Forms.Padding(4);
+            this.tbOgrAd.Name = "tbOgrAd";
+            this.tbOgrAd.Size = new System.Drawing.Size(187, 22);
+            this.tbOgrAd.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(268, 59);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 16);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Öğrenci Adı";
+            // 
+            // tbOgrNo
+            // 
+            this.tbOgrNo.Location = new System.Drawing.Point(389, 20);
+            this.tbOgrNo.Margin = new System.Windows.Forms.Padding(4);
+            this.tbOgrNo.Name = "tbOgrNo";
+            this.tbOgrNo.Size = new System.Drawing.Size(187, 22);
+            this.tbOgrNo.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(268, 24);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 16);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Öğrenci No";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnTopluSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.cbxSinif);
+            this.Controls.Add(this.cbxDers);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbOgrAd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbOgrNo);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnTopluSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ComboBox cbxSinif;
+        private System.Windows.Forms.ComboBox cbxDers;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbOgrAd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbOgrNo;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ OgrenciSistem/OgrenciSistem/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OgrenciSistem
+{
+    public partial class Form1 : Form
+    {
+        List<Ogrenci> ogrenciler = new List<Ogrenci>();
+        int gnclleIdx = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadDers()
+        {
+            cbxDers.Items.Add("Matematik");
+            cbxDers.Items.Add("Kimya");
+            cbxDers.Items.Add("Biyoloji");
+            cbxDers.Items.Add("Edebiyat");
+            cbxDers.Items.Add("Tarih");
+            cbxDers.Items.Add("Fizik");
+        }
+
+        private void LoadSinif()
+        {
+            cbxSinif.Items.Add("215");
+            cbxSinif.Items.Add("216");
+            cbxSinif.Items.Add("315");
+            cbxSinif.Items.Add("316");
+            cbxSinif.Items.Add("415");
+            cbxSinif.Items.Add("416");
+        }
+
+        private void ListViewDizayn()
+        {
+            listView1.View = View.Details;
+            listView1.FullRowSelect = true;
+            listView1.Columns.Add(label4.Text, 100);
+            listView1.Columns.Add(label5.Text, 100);
+            listView1.Columns.Add(label7.Text, 100);
+            listView1.Columns.Add("Not Ortalaması", 100);
+            listView1.Columns.Add("Durum", 100);
+        }
+
+        private void OgrenciEkle()
+        {
+            Random random = new Random();
+            var ogrenci = new Ogrenci();
+
+            if (ogrenciler.Count == 0 || OkulNoKontrol(int.Parse(tbOgrNo.Text)))
+            {
+                ogrenci.OgrenciEkle(int.Parse(tbOgrNo.Text), tbOgrAd.Text, cbxDers.Text, cbxSinif.Text, random.Next(1, 100));
+                ogrenciler.Add(ogrenci);
+                ListViewEkle(ogrenci);
+                Temizle();
+            }
+            else
+            {
+                foreach (var ogr in ogrenciler)
+                {
+                    if (ogr.OgrenciNo == int.Parse(tbOgrNo.Text))
+                    {
+                        if (ogr.OgrenciAdSoyad == tbOgrAd.Text)
+                        {
+                            if (ogr.OgrenciDers.Count < 5)
+                            {
+                                if (ogr.DersKontrol(cbxDers.Text, ogr))
+                                {
+                                    ogr.OgrenciDers.Add(cbxDers.Text);
+                                    ogr.Notlar.Add(random.Next(1, 100));
+                                    ListViewGuncelle(ogr);
+                                    break;
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Bir Öğrenci Aynı Dersi 2 kere Alamaz!!\nLütfen Başka Ders Seçiniz!!");
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show(ogr.OgrenciAdSoyad + " isimli öğrenci maksimum ders sayısına ulaşmıştır");
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Bu numaraya sahip başka öğrenci bulunmaktadır!!\nLütfen farklı bir numara girin..");
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Temizle()
+        {
+            tbOgrNo.Clear();
+            tbOgrAd.Clear();
+            cbxDers.Text = "";
+            cbxSinif.Text = "";
+        }
+
+        private void ListViewEkle(Ogrenci ogrenci)
+        {
+            string[] row = { ogrenci.OgrenciNo.ToString(), ogrenci.OgrenciAdSoyad, ogrenci.Sinif, OrtalamaHesapla(ogrenci).ToString(), (OrtalamaHesapla(ogrenci) > 50 ? "GEÇTİ" : "KALDI") };
+            ListViewItem lvi = new ListViewItem(row);
+            listView1.Items.Add(lvi);
+
+        }
+
+        private void ListViewGuncelle(Ogrenci ogr)
+        {
+
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                ListViewItem item = listView1.Items[i];
+                if (item.SubItems[0].Text == ogr.OgrenciNo.ToString())
+                {
+                    string[] row = { ogr.OgrenciNo.ToString(), ogr.OgrenciAdSoyad, ogr.Sinif, OrtalamaHesapla(ogr).ToString(), (OrtalamaHesapla(ogr) > 50 ? "GEÇTİ" : "KALDI") };
+                    ListViewItem lvi = new ListViewItem(row);
+                    listView1.Items[i] = lvi;
+                    break;
+                }
+            }
+
+        }
+
+        private static double OrtalamaHesapla(Ogrenci ogrenci)
+        {
+            double toplam = 0;
+            foreach (var item in ogrenci.Notlar)
+            {
+                toplam += item;
+            }
+            return toplam / ogrenci.OgrenciDers.Count;
+        }
+
+        public bool OkulNoKontrol(int okulNo)
+        {
+            foreach (var item in ogrenciler)
+            {
+                if (item.OgrenciNo == okulNo)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListViewDizayn();
+            LoadSinif();
+            LoadDers();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            OgrenciEkle();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            for (int i = listView1.Items.Count - 1; i >= 0; i--)
+            {
+                if (listView1.Items[i].Selected)
+                {
+                    listView1.Items[i].Remove();
+                    ogrenciler.RemoveAt(i);
+                }
+            }
+            Temizle();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            listView1.Items[gnclleIdx].SubItems[0].Text = tbOgrNo.Text;
+            listView1.Items[gnclleIdx].SubItems[1].Text = tbOgrAd.Text;
+            listView1.Items[gnclleIdx].SubItems[2].Text = cbxSinif.Text;
+
+            foreach (var item in ogrenciler)
+            {
+                if (item.OgrenciNo == int.Parse(listView1.Items[gnclleIdx].SubItems[0].Text))
+                {
+                    item.OgrenciNo = int.Parse(tbOgrNo.Text);
+                    item.OgrenciAdSoyad = tbOgrAd.Text;
+                    item.Sinif = cbxSinif.Text;
+                    break;
+                }
+            }
+            gnclleIdx = 0;
+            Temizle();
+        }
+
+        private void listView1_Click(object sender, EventArgs e)
+        {
+            for (int i = listView1.Items.Count - 1; i >= 0; i--)
+            {
+                if (listView1.Items[i].Selected)
+                {
+                    gnclleIdx = i;
+                    tbOgrNo.Text = listView1.Items[i].SubItems[0].Text;
+                    tbOgrAd.Text = listView1.Items[i].SubItems[1].Text;
+                    cbxSinif.Text = listView1.Items[i].SubItems[2].Text;
+                    break;
+                }
+            }
+        }
+    }
+}
